1faa15151d69abbc2d15806515909ffd7212e7c6
Joshua Freedman <npjoshf@gmail.com>
2015-10-07 21:45:26 -0700

    Lots o stuff... Started basic ifs, and maths...

--- main.cpp
+++ main.cpp
@@ -30,50 +30,158 @@ std::list<std::string> split(const std::string &s, char delim) {
     return elems;
 }
 
+template<typename T>
+std::string to_string(T const &value) {
+    stringstream sstr;
+    sstr << value;
+    return sstr.str();
+}
+
 int main() {
 
-    char *code = (char *) "var hi\n"
-            "set hi 5";
+//    char *code = (char *) "var hi\n"
+//            "var hello\n"
+//            "var ans\n"
+//            "set hi 5\n"
+//            "set hello 10\n"
+//            "mult ans hi hello\n"
+//            "sub ans ans hi\n"
+//            "printvar ans\n"
+//            "input hi\n"
+//            "printvar hi";
+
+    //TODO Functions/Methods
+
+    // This code waits for use to enter two numbers, then multiplies together and outputs to screen
+    char *multiplication_code = (char *) "var num1;var num2;var ans;input num1;input num2;mult ans num1 num2;printvar ans;";
+    char *addition_code = (char *) "var num1;var num2;var ans;input num1;input num2;add ans num1 num2;printvar ans;";
+    char *subtraction_code = (char *) "var num1;var num2;var ans;input num1;input num2;sub ans num1 num2;printvar ans;";
+    char *division_code = (char *) "var num1;var num2;var ans;input num1;input num2;div ans num1 num2;printvar ans;";
+
+
+    //first input is num1, second is operator (+-*/), third is num2
+    char *calc_code = (char *) "var vdiv;set vdiv /;var vmult;set vmult *;var vadd;set vadd +;var vmin;set vmin -;var num1;var num2;var opt;var ans;input num1;input opt;input num2;ifeq opt vadd;add ans num1 num2;endif;ifeq opt vmin;sub ans num1 num2;endif;ifeq opt vmult;mult ans num1 num2;endif;ifeq opt vdiv;div ans num1 num2;endif;printvar ans;";
+
+//    char *if_code = (char *) "var div;set div /;";
+
+
+    char *code = calc_code;
 
     list<string> list1;
-    list1 = split(code, '\n');
+    list1 = split(code, ';');
 
     run(list1);
 
-    cout << list1.size() << endl;
-
     system("pause");
+    exit(0);
     return 0;
 }
+
+int ifLevel = 0;
 
 void run(std::list<string> list1) {
     unordered_map<string, string> varMap;
     for (std::list<string>::iterator list_iter = list1.begin(); list_iter != list1.end(); list_iter++) {
+        std::cout << "\t" << *list_iter << " : " << ifLevel << endl;
         list<string> parts = split(*list_iter, ' ');
-        if (*parts.begin() == "var") {
-            parts.pop_front();
-            cerr << parts.size() << endl;
-            varMap.insert(make_pair(*parts.begin(), ""));
-        }
-        if (*parts.begin() == "set") {
-            list<string> parts2 = split(*list_iter, ' ');
-            parts2.pop_front();
-            string dev[3];
-            int i = 0;
-            for (auto &itr : parts2) {
-                dev[i] = itr;
-                i++;
+        if (ifLevel < 1) {
+            if (*parts.begin() == "var") {
+                parts.pop_front();
+                varMap.insert(make_pair(*parts.begin(), ""));
             }
-            auto it = varMap.find(dev[0]);
-            if (it != varMap.end())
-                it->second = dev[1];
+            if (*parts.begin() == "set") {
+                list<string> parts2 = split(*list_iter, ' ');
+                parts2.pop_front();
+                string dev[3];
+                int i = 0;
+                for (auto &itr : parts2) {
+                    dev[i] = itr;
+                    i++;
+                }
+                auto it = varMap.find(dev[0]);
+                if (it != varMap.end())
+                    it->second = dev[1];
+            }
+            if (*parts.begin() == "printvar") {
+                parts.pop_front();
+                cout << "Value of " << *parts.begin() << ": " << varMap[*parts.begin()] << endl;
+            }
+            if (*parts.begin() == "add") {
+                parts.pop_front();
+                string var = *parts.begin();
+                parts.pop_front();
+                string v2 = *parts.begin();
+                parts.pop_front();
+                string v3 = *parts.begin();
+                double nVal = strtod(varMap[v2].c_str(), NULL) + strtod(varMap[v3].c_str(), NULL);
+                auto it = varMap.find(var);
+                if (it != varMap.end())
+                    it->second = to_string(nVal);
+            }
+            if (*parts.begin() == "mult") {
+                parts.pop_front();
+                string var = *parts.begin();
+                parts.pop_front();
+                string v2 = *parts.begin();
+                parts.pop_front();
+                string v3 = *parts.begin();
+                double nVal = strtod(varMap[v2].c_str(), NULL) * strtod(varMap[v3].c_str(), NULL);
+                auto it = varMap.find(var);
+                if (it != varMap.end())
+                    it->second = to_string(nVal);
+            }
+            if (*parts.begin() == "div") {
+                parts.pop_front();
+                string var = *parts.begin();
+                parts.pop_front();
+                string v2 = *parts.begin();
+                parts.pop_front();
+                string v3 = *parts.begin();
+                double nVal = strtod(varMap[v2].c_str(), NULL) / strtod(varMap[v3].c_str(), NULL);
+                auto it = varMap.find(var);
+                if (it != varMap.end())
+                    it->second = to_string(nVal);
+            }
+            if (*parts.begin() == "sub") {
+                parts.pop_front();
+                string var = *parts.begin();
+                parts.pop_front();
+                string v2 = *parts.begin();
+                parts.pop_front();
+                string v3 = *parts.begin();
+                double nVal = strtod(varMap[v2].c_str(), NULL) - strtod(varMap[v3].c_str(), NULL);
+                auto it = varMap.find(var);
+                if (it != varMap.end())
+                    it->second = to_string(nVal);
+            }
+            if (*parts.begin() == "input") {
+                parts.pop_front();
+                string var = *parts.begin();
+
+                string uInput;
+                cin >> uInput;
+
+                auto it = varMap.find(var);
+                if (it != varMap.end())
+                    it->second = uInput;
+            }
+        } else {
+            if (*parts.begin() == "endif") {
+                ifLevel--;
+            }
         }
-        std::cout << *list_iter << endl;
+        if (*parts.begin() == "ifeq") {
+            parts.pop_front();
+            string var = *parts.begin();
+            parts.pop_front();
+            string var2 = *parts.begin();
+            cout << "\t\t" << varMap[var] << " : " << varMap[var2] << " : " << (varMap[var] == varMap[var2]) << endl;
+            if (varMap[var] != varMap[var2]) ifLevel++;
+        }
+
     }
 
-    for (int i = 0; i < 10; i++)cout << endl;
-    for (auto &itr : varMap) {
-        cout << itr.first << ": " << itr.second << endl;
-    }
+    for (int i = 0; i < 10; i++)
+        cout << endl;
 
 }
